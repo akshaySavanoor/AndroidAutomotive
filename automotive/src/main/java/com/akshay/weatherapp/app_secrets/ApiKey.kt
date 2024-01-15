2df2b7259b7aa2d707dfb819222fea89711c4a52
Akshay Savanoor <akshay.savanoor@codecraft.co.in>
2024-01-15 09:35:27 +0530

    Delete - Removed ApiKey.kt

--- automotive/src/main/java/com/akshay/weatherapp/app_secrets/ApiKey.kt
+++
@@ -1,6 +0,0 @@
-package com.akshay.weatherapp.app_secrets
-
-object ApiKey {
-    const val API_KEY = "31844afd56ba8a2d3aa7798a6e799eb0"
-    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
-}
